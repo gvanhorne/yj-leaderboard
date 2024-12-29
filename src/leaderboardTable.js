bb5ac8bfdfa4c038655dd4e698b2d8dcd976ad3b
Gregory Van Horne <gvanhorne@telus.net>
2024-12-29 15:11:06 -0800

    Change name of leaderboard table component

--- src/leaderboardTable.js
+++ src/leaderboardTable.js
@@ -3,14 +3,12 @@ import { table } from 'npm:@observablehq/inputs';
 export default function leaderboardTable(data) {
     return table(data, {
       columns: [
-        "player",
-        "date",
+        "name",
         "qualifying_points",
         "tiebreaker_points",
       ],
       header: {
-        player: "Player Name",
-        date: "Date",
+        name: "Player Name",
         qualifying_points: "Qualifying Points",
         tiebreaker_points: "Tiebreaker Points",
       }
