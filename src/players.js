02cccf401bbbece72a1948602417c447dc062866
Gregory Van Horne <gvanhorne@telus.net>
2024-12-29 15:11:12 -0800

    Add players data

---
+++ src/players.js
@@ -0,0 +1,14 @@
+const players = [
+    { name: 'John Doe', qualifying_points: 14, tiebreaker_points: 3 },
+    { name: 'Alice Brown', qualifying_points: 14, tiebreaker_points: 5 },
+    { name: 'Bob Johnson', qualifying_points: 8, tiebreaker_points: 2 },
+    { name: 'Jane Smith', qualifying_points: 3, tiebreaker_points: 3 },
+    { name: 'Diana Morrison', qualifying_points: 10, tiebreaker_points: 4 },
+    { name: 'Leah Mathis', qualifying_points: 12, tiebreaker_points: 6 },
+    { name: 'Brandon Martin', qualifying_points: 7, tiebreaker_points: 2 },
+    { name: 'Karen Reid', qualifying_points: 5, tiebreaker_points: 1 },
+    { name: 'Sarah Roberts', qualifying_points: 9, tiebreaker_points: 3 },
+    { name: 'Paul Hamilton', qualifying_points: 11, tiebreaker_points: 4 }
+];
+
+export default players;
